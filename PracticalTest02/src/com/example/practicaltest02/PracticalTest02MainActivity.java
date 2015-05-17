99b98f18fcac3e93e52e0d74c6d976ef11c0bf83
radupoenaru1992 <radupoenaru1992@yahoo.com>
2015-05-18 01:08:06 +0300

    tasks for PT02

---
+++ PracticalTest02/src/com/example/practicaltest02/PracticalTest02MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.practicaltest02;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.view.Menu;
+
+public class PracticalTest02MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practical_test02_main);
+        
+
+        FragmentManager fragmentManager = getFragmentManager();
+		FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+		fragmentTransaction.add(R.id.fragmentServer, new ServerFragment());
+		fragmentTransaction.add(R.id.fragmentClient, new ClientFragment());
+		fragmentTransaction.commit();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.practical_test02_main, menu);
+        return true;
+    }
+    
+}
